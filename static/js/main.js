97ff978289c98a7eed30b1af04b254eedefbcaa0
ytnoonty <anthonyjjoy@gmail.com>
2018-07-21 20:29:02 -0400

    updated everything

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,73 @@
 let flashMsgDiv = document.getElementById('flash-msg-div');
 let updateProcessPrint = document.getElementById('update-tv-screen');
 
-updateProcessPrint.addEventListener('click', function(e){
-  window.location.reload();
-  screenRefresh();
-  loadBeerlist();
-});
+if (updateProcessPrint != null) {
+  updateProcessPrint.addEventListener('click', function(e){
+    window.location.reload();
+    screenRefresh();
+    loadBeerlist();
+    // loadTicker();
+  });
+}
 
-
+// loadTicker();
 loadBeerlist();
-// loadScreenlist();
+// screenRefresh();
+
+//////////////////////////////////////////////////////
+// START loadTicker()
+/////////////////////////////////////////////////////
+// function loadTicker(e) {
+//   // console.log('testing function works');
+//   const xhr = new XMLHttpRequest();
+//   // console.log('READYSTATE1', xhr.readyState);
+//   //Open
+//   // xhr.open('POST', '/update', true);
+//   xhr.open('POST', '/update', true);
+//   // console.log('READYSTATE2', xhr.readyState);
+//   // console.log('STATUS before onload', this.status);
+//   // xhr.onload
+//   xhr.onload = function(){
+//     // console.log('READYSTATE3', xhr.readyState);
+//     // console.log('STATUS after onload', this.status);
+//     if (this.status === 200) {
+//       // console.log(this.responseText);
+//
+//       let data = JSON.parse(this.responseText);
+//       let data1721 = data.slice(16,21);
+//       let tickerhtml = '';
+//       let btsComingSoon = document.querySelector(".ticker");
+//       tickerhtml += `<p class="p-bts-coming-soon bold-font txt-clr-ylw">
+//       <span>Beer 'O the Month:</span>
+//       <span class="card-img">${data1721[0].name}</span>
+//       <span>Tapping Soon:
+//         <span class="card-img-before">${data1721[1].name}</span>
+//         <span class="card-img-before">${data1721[2].name}</span>
+//         <span class="card-img-before">${data1721[3].name}</span>
+//         <span class="card-img">${data1721[4].name}</span>
+//       </span>
+//       </p>`;
+//       if (btsComingSoon != null) {
+//         // tickerhtml = data1721[0].name;
+//         // console.log(tickerhtml);
+//         btsComingSoon.innerHTML = tickerhtml;
+//       }
+//
+//     }
+//   }
+//
+//   xhr.send();
+//   setTimeout(loadTicker,250);
+//   e.preventDefault();
+// }
+//////////////////////////////////////////////////////
+// END loadTicker()
+/////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////
+// START screenRefresh()
+/////////////////////////////////////////////////////
 
 function screenRefresh(){
   let currentTime = new Date();
@@ -17,18 +75,18 @@ function screenRefresh(){
   let min = currentTime.getMinutes();
   let sec = currentTime.getSeconds();
   let millSec = currentTime.getMilliseconds();
-  console.log('hour is: ' + hour);
-  console.log('min is: ' + min);
-  console.log('sec is: ' + sec);
-  console.log('millSec is: ' + millSec);
+  // console.log('hour is: ' + hour);
+  // console.log('min is: ' + min);
+  // console.log('sec is: ' + sec);
+  // console.log('millSec is: ' + millSec);
   if ((hour == 6 || hour == 10 || hour == 14 || hour == 16 || hour == 18 || hour == 20 || hour == 22 || hour == 24) && min == 1 && sec == 1 && millSec < 250){
     window.location.reload();
-    console.log('in the new function and minutes is: ' + min);
-    console.log('hour is: ' + hour);
-    console.log('min is: ' + min);
-    console.log('sec is: ' + sec);
-    console.log('millSec is: ' + millSec);
-    console.log('************************SCREEN UPDATED*******************');
+    // console.log('in the new function and minutes is: ' + min);
+    // console.log('hour is: ' + hour);
+    // console.log('min is: ' + min);
+    // console.log('sec is: ' + sec);
+    // console.log('millSec is: ' + millSec);
+    // console.log('************************SCREEN UPDATED*******************');
   }
   // if (sec == 31 && millSec < 250){
   //   window.location.reload();
@@ -38,7 +96,13 @@ function screenRefresh(){
   //   console.log('************************SCREEN UPDATED*******************');
   // }
 }
+//////////////////////////////////////////////////////
+// END screenRefresh()
+/////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////
+// START loadBeerlist()
+/////////////////////////////////////////////////////
 function loadBeerlist(e) {
   // console.log('testing function works');
 
@@ -54,68 +118,76 @@ function loadBeerlist(e) {
   // xhr.onload
   xhr.onload = function(){
     // console.log('READYSTATE3', xhr.readyState);
-
     // console.log('STATUS after onload', this.status);
     if (this.status === 200) {
       // console.log(this.responseText);
-
-      let beerListDiv = document.getElementById('beer-list-div');
-      let ul = document.createElement('ul');
+      // let ul = document.createElement('ul');
       //use this as a reference
-      ul.setAttribute("id", "this-is-the-id");
-      ul.classList.add = 'list-group-flush';
-
+      // ul.setAttribute("id", "this-is-the-id");
+      // ul.classList.add = 'list-group-flush';
       let data = JSON.parse(this.responseText);
       let data0116 = data.slice(0,16);
       let data1721 = data.slice(16,21);
-
       let data0108 = data.slice(0,8);
       let data0816 = data.slice(8,16);
 
-
-
-      // let htmlStuff = `${data0116[0].id}. ${data0116[0].name}`;
-      // if (beerListDiv !== null) {
-      //   beerListDiv.innerHTML = htmlStuff;
-      // }
 ////////////////////////////////////////////////////////////
-// PROCCESS_PRINT.HTML UPDATE
+// START BEERS_PRINT.HTML UPDATE
 ////////////////////////////////////////////////////////////
-      let htmlStuff = '';
-      htmlStuff = '';
+      let beerListDivBP = document.getElementsByClassName('beer-list-div-bp');
+      // beerListDivBP[0].innerHTML = "";
+      // console.log(beerListDiv);
+      let htmlStuffBP = '';
 
-      htmlStuff += `<ul id="beer-list-loop" class='list-group-flush'>`;
-
+      htmlStuffBP += `<ul id="" class='list-group-flush'>`;
       data0116.forEach(function(beer){
-        let li = document.createElement('li');
-        // li.className = 'list-group-item';
-        htmlStuff += `<li id="list-group-item-pp" class='list-group-item'>${beer.id}.  <span class="larger-text text-color-grn">${beer.name}</span> - ${beer.abv}% ABV - ${beer.ibu} IBU - ${beer.location} - <span class="italic-font">${beer.brewery}</span></li>`;
+        htmlStuffBP += `<li id="" class='list-group-item'>${beer.id}.  <span class="larger-text txt-clr-grn">${beer.name}</span> - ${beer.style} - ${beer.abv}% ABV - ${beer.ibu} IBU - ${beer.location} - <span class="italic-font">${beer.brewery}</span></li>`;
       });
-
-      htmlStuff += `</ul>`;
-      // ul.innerHTML = htmlStuff;
-      if (beerListDiv !== null) {
-        console.log('htmlStuff');
-        beerListDiv.innerHTML = htmlStuff;
+      htmlStuffBP += `</ul>`;
+      if (beerListDivBP[0] !== undefined) {
+        beerListDivBP[0].innerHTML = htmlStuffBP;
       }
 
-      let monthP = document.getElementById('month-p');
-      // console.log(document.getElementById('month-p'));
-      if (monthP !== null) {
-        monthP.innerHTML = `<span class="mx-3 larger-text text-color">${data1721[0].name}</span>`;
+      let monthPBP = document.getElementsByClassName('month-p-bp');
+      if (monthPBP[0] !== undefined) {
+        monthPBP[0].innerHTML = `<span class="mx-3 larger-text txt-clr-grn">${data1721[0].name}</span>`;
       }
 
-      let comingsoonP = document.getElementById('comingsoon-p');
-      // console.log(document.getElementById('comingsoon-p'));
-      if (comingsoonP !== null) {
-        comingsoonP.innerHTML = `<span class="mx-4">${data1721[1].name}</span>
-        <span class="mx-4">${data1721[2].name}</span>
-        <span class="mx-4">${data1721[3].name}</span>
-        <span class="mx-4">${data1721[4].name}</span>`;
+      let comingsoonPBP = document.getElementById('comingsoon-p-bp');
+      if (comingsoonPBP !== null) {
+        comingsoonPBP.innerHTML = `<span class="mx-4 larger-text txt-clr-grn">${data1721[1].name}</span>
+        <span class="mx-4 larger-text txt-clr-grn">${data1721[2].name}</span>
+        <span class="mx-4 larger-text txt-clr-grn">${data1721[3].name}</span>
+        <span class="mx-4 larger-text txt-clr-grn">${data1721[4].name}</span>`;
       }
-      ////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////
-// PROCCESS_PRINT.HTML UPDATE
+// END BEERS_PRINT.HTML UPDATE
+// /////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// START PROCCESS_PRINT.HTML UPDATE
+////////////////////////////////////////////////////////////
+      let beerListDivPP = document.getElementsByClassName('beer-list-div-pp');
+      // beerListDivPP[0].innerHTML = "";
+      // console.log(beerListDiv);
+      let htmlStuffPP = '';
+
+      htmlStuffPP += `<ul id="" class='beer-list-loop-pp list-group-flush'>`;
+      data0116.forEach(function(beer){
+        htmlStuffPP += `<li id="" class='list-group-item-pp list-group-item'>${beer.id}.  <span class="larger-text txt-clr-grn">${beer.name}</span> - ${beer.style} - ${beer.abv}% ABV - ${beer.ibu} IBU - ${beer.location} - <span class="italic-font">${beer.brewery}</span></li>`;
+      });
+      htmlStuffPP += `</ul>`;
+      if (beerListDivPP[0] !== undefined) {
+        beerListDivPP[0].innerHTML = htmlStuffPP;
+      }
+
+      let monthPPP = document.getElementsByClassName('month-p-pp');
+      if (monthPPP[0] !== undefined) {
+        monthPPP[0].innerHTML = `<span class="mx-3 larger-text txt-clr-grn">${data1721[0].name}</span>`;
+      }
+
+////////////////////////////////////////////////////////////
+// END PROCCESS_PRINT.HTML UPDATE
 // /////////////////////////////////////////////
 
 
@@ -317,7 +389,7 @@ example6HTML = `
               <tr>
                 <h1 class="italic-font bold-font txt-clr-ylw left-spacer no-btm-margin"><span class="">${beer.name}</span></h1>
               </tr>
-              <tr class="font-med left-spacer">
+              <tr class="font-med left-spacer beer-screen-tr">
                 <td class="bold-font w-third">${beer.style}</td>
                 <td class="w-forth">ABV ${beer.abv}%</td>
                 <td class="italic-font bold-font w-third">${beer.brewery}</td>
@@ -339,7 +411,7 @@ example6HTML += `
               <tr>
                 <h1 class="italic-font bold-font txt-clr-ylw left-spacer no-btm-margin"><span class="">${beer.name}</span></h1>
               </tr>
-              <tr class="font-med left-spacer">
+              <tr class="font-med left-spacer beer-screen-tr">
                 <td class="bold-font w-third">${beer.style}</td>
                 <td class="w-forth">ABV ${beer.abv}%</td>
                 <td class="italic-font bold-font w-third">${beer.brewery}</td>
@@ -352,63 +424,37 @@ example6HTML += `
     </div>
   </div>
 </div>`;
-// example6HTML = `
-// <div class="row">
-//   <div class="col-lg">
-//     <div class="list-group">
-//       <ul class="list-group-flush list-group-bts">`;
-//       //USED FOR TESTING THE ajax update
-//       //uncomment line below and reload browser or reboot pi
-//       // example6HTML += `<div></div>`;
-//         data0108.forEach(function(beer){
-//           example6HTML += `
-//           <li class="card card-img-before">
-//             <table>
-//               <tr>
-//                 <h1 class="italic-font bold-font txt-clr-ylw"><span class="underline">${beer.name}</span></h1>
-//               </tr>
-//               <tr class="font-med">
-//                 <td class="bold-font w-third">${beer.style}</td>
-//                 <td class="w-forth">ABV ${beer.abv}%</td>
-//                 <td class="italic-font bold-font w-third">${beer.brewery}</td>
-//               </tr>
-//             </table>
-//           </li>`
-//         });
-// example6HTML += `
-//       </ul>
-//     </div>
-//   </div>
-//   <div class="col-lg">
-//     <div class="list-group">
-//       <ul class="list-group-flush list-group-bts">`;
-//         data0816.forEach(function(beer){
-//           example6HTML += `
-//           <li class="card card-img-before">
-//             <table>
-//               <tr>
-//                 <h1 class="italic-font bold-font txt-clr-ylw"><span class="underline">${beer.name}</span></h1>
-//               </tr>
-//               <tr class="font-med">
-//                 <td class="bold-font w-third">${beer.style}</td>
-//                 <td class="w-forth">ABV ${beer.abv}%</td>
-//                 <td class="italic-font bold-font w-third">${beer.brewery}</td>
-//               </tr>
-//             </table>
-//           </li>`
-//         });
-// example6HTML += `
-//       </ul>
-//     </div>
-//   </div>
-// </div>`;
 if (example6 !== null) {
+  // console.log(example6HTML);
   example6.innerHTML = example6HTML;
 }
 /////////////////////////////////////
 // END example6
 /////////////////////////////////////
 
+/////////////////////////////////////////////
+// BEGIN Ticker
+/////////////////////////////////////////////
+
+let tickerhtml = '';
+let btsComingSoon = document.querySelector(".ticker");
+tickerhtml += `<p class="p-bts-coming-soon">
+<span class="txt-clr-grn-shdw">Beer 'O the Month:</span>
+<span class="card-img bold-font italic-font txt-clr-ylw">${data1721[0].name}</span>
+<span class="txt-clr-grn-shdw">Tapping Soon:
+  <span class="card-img-before bold-font italic-font txt-clr-ylw">${data1721[1].name}</span>
+  <span class="card-img-before bold-font italic-font txt-clr-ylw">${data1721[2].name}</span>
+  <span class="card-img-before bold-font italic-font txt-clr-ylw">${data1721[3].name}</span>
+  <span class="card-img bold-font italic-font txt-clr-ylw">${data1721[4].name}</span>
+</span>
+</p>`;
+if (btsComingSoon != null) {
+  // console.log(tickerhtml);
+  btsComingSoon.innerHTML = tickerhtml;
+}
+/////////////////////////////////////////////
+// END Ticker
+/////////////////////////////////////////////
 
 /////////////////////////////////////////////
 /////////////////////////////////////////////
@@ -418,15 +464,20 @@ if (example6 !== null) {
 
   xhr.send();
   screenRefresh();
-  setTimeout(loadBeerlist,250);
-  // setTimeout(loadScreenlist,1000);
-
-
+  // loadTicker();
   setTimeout(function(){
     flashMsgDiv.style.display = 'none';
   },2500);
+
+  setTimeout(loadBeerlist,250);
+
   e.preventDefault();
 }
+
+//////////////////////////////////////////////////////
+// END loadBeerlist()
+/////////////////////////////////////////////////////
+
 
 
 
